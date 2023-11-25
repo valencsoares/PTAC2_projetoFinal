36bb54998a107b9557ab61b33b479009016fb2f6
valencsoares <pvcampossoares558@gmail.com>
2023-11-25 16:39:33 -0400

    Criado o componente para o cadastro, importado o menu nele, feito uma pequena estilização para não ter margem e importado essa lista na página de cadastro

---
+++ PTAC-projeto-final-main/src/Cadastrar.jsx
@@ -0,0 +1,9 @@
+import ListaCadastro from "./componentes/ListaCadastro.jsx";
+
+export default function Cadastrar(){
+    return(
+        <body>
+            <ListaCadastro/>
+        </body>
+    )
+}
---
+++ PTAC-projeto-final-main/src/componentes/ListaCadastro.jsx
@@ -0,0 +1,13 @@
+import Menu from "./Menu.jsx";
+import "./../style.css";
+
+export default function ListaCadastro(){
+    return(
+        <body>
+            <Menu/>
+            <div class="cadastro">
+                <h1>Cadastre já!</h1>
+            </div>
+        </body>
+     );
+}
