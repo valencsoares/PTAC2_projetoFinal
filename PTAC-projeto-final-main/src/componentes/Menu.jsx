efabfa63cec1f46e57e12360fc03f98122743dd6
valencsoares <pvcampossoares558@gmail.com>
2023-11-25 16:37:07 -0400

    Alterado a navbar para um menu vertical na lateral, e alterado css, especificando tamanho do menu, a estilização dos links e da lista em que eles estão

--- PTAC-projeto-final-main/src/componentes/Menu.jsx
+++ PTAC-projeto-final-main/src/componentes/Menu.jsx
@@ -1,15 +1,12 @@
 import "./../style.css";
 export default function Menu(){
     return(
-        <header class="header">
-            <nav>
-                <div class="grid">
-                    <div class="navbar">
-                        <a class="link" aria-current="page" href="/">Destaques</a>
-                        <a class="link" aria-current="page" href="/">Cadastrar</a>
-                    </div>
-                </div>
-            </nav>
-        </header>
+            <div class="menu">
+                <ul>
+                    <li><a href="/">Página principal</a></li>
+                    <li><a href="/destaques">Destaques</a></li>
+                    <li><a href="/cadastrar">Cadastrar</a></li>
+                </ul>
+            </div>
      );
 }
