a63d21d4639c39fbc46e64cf490cca62aac4693b
valencsoares <pvcampossoares558@gmail.com>
2023-11-28 08:59:22 -0400

    Feito a função de salvar os videos, na qual será acionada quando clicar no botão

--- PTAC-projeto-final-main/src/componentes/ListaCadastro.jsx
+++ PTAC-projeto-final-main/src/componentes/ListaCadastro.jsx
@@ -8,6 +8,19 @@ export default function ListaCadastro(){
     const [descricao, setDescricao] = useState("");
     const [id, setId] = useState(1);
     /* estados */
+
+    const salvar = (e) =>{
+        e.preventDefault(); //não recarregar a página
+        setListaVideos([...listaVideos, { //"..." serve para pegar tudo que tem na lista e add algo a mais
+            titulo:titulo, descricao:descricao, id:id
+        }]); 
+        //para fazer com que apague as informações do input quando chamar a função
+        setTitulo("")
+        setDescricao("")
+        setId(id + 1)
+        console.log(listaVideos)
+    };
+
     return(
         <body>
             <Menu/>
