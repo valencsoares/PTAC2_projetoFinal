29e0b127fee1ec09dd4d947200db0d378d6282b9
valencsoares <pvcampossoares558@gmail.com>
2023-12-01 17:45:26 -0400

    Adicionado o card na tela

--- PTAC-projeto-final-main/src/Cadastrar.jsx
+++ PTAC-projeto-final-main/src/Cadastrar.jsx
@@ -30,11 +30,12 @@ export default function Cadastrar(){
     };
 
     return(
-        <body>
-        <Menu/>
+        <div>
+            <Menu/>
         <div class="cadastro">
         <h1>Lista de livros para ler!</h1>
         <form onSubmit={salvar}>
+            <input placeholder="adicione a url do vídeo" type="text" value={video} onChange={(e)=>{setVideo(e.target.value)}}/>
             <input placeholder="adicione o título" type="text" value={titulo} onChange={(e)=>{setTitulo(e.target.value)}}/>
             <input placeholder="adicione a descrição" type="text" value={descricao} onChange={(e)=>{setDescricao(e.target.value)}}/>
             <div>
@@ -42,6 +43,27 @@ export default function Cadastrar(){
             </div>    
         </form>
         </div>
-    </body>
-    )
-}
+
+        {listaVideos.map((ativ) =>
+        <div class="cardd" key={ativ.id}>
+        {/* <iframe
+            width="300"
+            height="200"
+            src={`https://www.youtube.com/embed/${video.url.slice(17)}`}
+            title="Video"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+        ></iframe> */}
+        <div class="card">
+            <div>
+                <Link to={`/detalhe/${ativ.id}`}>
+                    <p class="titulo">{ativ.titulo}</p>
+                </Link> {/* //essa linha cria um link que leva o usuário para a rota prescrita, onde :identidade será substituído pelo valor da propriedade identidade da atividade atual.  */}
+                <p><span>Descrição:</span> {ativ.descricao}</p>
+            </div>
+        </div>
+    </div>
+    )}
+    </div>
+)}
