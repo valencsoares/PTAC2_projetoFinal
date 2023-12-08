3de21c6285d0d54ba5ee1f571b33ec53e80a6ae5
valencsoares <pvcampossoares558@gmail.com>
2023-12-08 17:11:25 -0400

    add código para o componente card para aparecer na home

--- PTAC-projeto-final-main/src/Home.jsx
+++ PTAC-projeto-final-main/src/Home.jsx
@@ -1,9 +1,14 @@
 import Menu from "./componentes/Menu.jsx";
+import Card from "./componentes/card.jsx";
 
 export default function Home(){
+    const listaVideosSalva = JSON.parse(localStorage.getItem("ListaVideos"))|| [];
     return(
         <body>
             <Menu/>
+            {listaVideosSalva.map((ativ) => <Card ativ = {ativ}/>)}
+
+
         </body> 
      );
 }
