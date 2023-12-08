bd40e2bea9abe1963200782304b87a4009d4b516
valencsoares <pvcampossoares558@gmail.com>
2023-12-08 17:10:51 -0400

    Criado o card de video como componente, e add o código para aparecer o video do youtube

--- PTAC-projeto-final-main/src/componentes/card.jsx
+++ PTAC-projeto-final-main/src/componentes/card.jsx
@@ -1,14 +1,25 @@
 import "./../style.css";
 
-export default function Card(){
-    <div class="cardd" key={ativ.id}>
+export default function Card({ativ}){
+const idDoVideo = ativ.url.slice(17)
+
+  return(
+  <div class="cardd" key={ativ.id}>
         <div class="card">
+        <iframe
+            width="300"
+            height="200"
+            src={`https://www.youtube.com/embed/${idDoVideo}`}
+            title="Video"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+        ></iframe>
             <div>
-                <Link to={`/detalhe/${ativ.id}`}>
-                    <p class="titulo">{ativ.titulo}</p>
-                </Link> {/* //essa linha cria um link que leva o usuário para a rota prescrita, onde :identidade será substituído pelo valor da propriedade identidade da atividade atual.  */}
+                <p class="titulo">{ativ.titulo}</p>
                 <p><span>Descrição:</span> {ativ.descricao}</p>
             </div>
         </div>
     </div>
+  )
 }
