77e410be3ade927fddd4704234aa0b687bcf067d
valencsoares <pvcampossoares558@gmail.com>
2023-12-08 17:11:44 -0400

    Criado a página de destaques e a rota dele no main

--- PTAC-projeto-final-main/src/main.jsx
+++ PTAC-projeto-final-main/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home.jsx'
 import Cadastrar from './Cadastrar.jsx'
+import Destaque from './Destaque.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/cadastrar" element={<Cadastrar/>}></Route>
+        <Route path="/destaques" element={<Destaque/>}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
