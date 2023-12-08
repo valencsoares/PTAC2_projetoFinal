e60632a4efb913824a5c8d7ceb0641ce0b3cf4f9
valencsoares <pvcampossoares558@gmail.com>
2023-12-08 17:10:10 -0400

    Removido os videos da aba de cadastrar

--- PTAC-projeto-final-main/src/Cadastrar.jsx
+++ PTAC-projeto-final-main/src/Cadastrar.jsx
@@ -43,27 +43,5 @@ export default function Cadastrar(){
             </div>    
         </form>
         </div>
-
-        {listaVideos.map((ativ) =>
-        <div class="cardd" key={ativ.id}>
-        {/* <iframe
-            width="300"
-            height="200"
-            src={`https://www.youtube.com/embed/${video.url.slice(17)}`}
-            title="Video"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-        ></iframe> */}
-        <div class="card">
-            <div>
-                <Link to={`/detalhe/${ativ.id}`}>
-                    <p class="titulo">{ativ.titulo}</p>
-                </Link> {/* //essa linha cria um link que leva o usuário para a rota prescrita, onde :identidade será substituído pelo valor da propriedade identidade da atividade atual.  */}
-                <p><span>Descrição:</span> {ativ.descricao}</p>
-            </div>
-        </div>
-    </div>
-    )}
     </div>
 )}
